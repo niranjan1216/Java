22aa21651798bbc5c5360d5ea81479cd65e969d3
P NIRANJAN KUMAR <83505439+niranjan1216@users.noreply.github.com>
2025-05-08 05:25:31 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -2,6 +2,8 @@ public class Simple
 {
 public static void main(String args[])
 {
-System.out.println("Welcome");
+  System.out.println("Welcome");
+  System.out.println("Welcome to Jenkins");
+  
 }
 }
