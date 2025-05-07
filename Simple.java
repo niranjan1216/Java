deb1f9d9d97603877dc6ce4bd143a70033d460c2
P NIRANJAN KUMAR <83505439+niranjan1216@users.noreply.github.com>
2025-05-08 05:29:38 +0530

    Updated Simple.java
    added one more print statement

--- Simple.java
+++ Simple.java
@@ -3,7 +3,7 @@ public class Simple
 public static void main(String args[])
 {
   System.out.println("Welcome");
-  System.out.println("Welcome to Jenkins");
+  System.out.println("Welcome to Jenkins 2");
   
 }
 }
