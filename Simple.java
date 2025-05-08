0e2db61f18d310cee57786fd0fb53a2a1ff04ba7
P NIRANJAN KUMAR <83505439+niranjan1216@users.noreply.github.com>
2025-05-08 05:45:39 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -3,7 +3,7 @@ public class Simple
 public static void main(String args[])
 {
   System.out.println("Welcome");
-  System.out.println("Welcome to Jenkins 3")
+  System.out.println("Welcome to Jenkins 3");
   
 }
 }
